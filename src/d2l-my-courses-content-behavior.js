b37a61217f0b19c938bbc6d149dea3ea0c049d18
Edward Gao <Edward.Gao@D2L.com>
2019-04-25 13:49:53 -0400

    DE33973-fix duplicate card issue

--- src/d2l-my-courses-content-behavior.js
+++ src/d2l-my-courses-content-behavior.js
@@ -270,6 +270,9 @@ D2L.MyCourses.MyCoursesContentBehaviorImpl = {
 		if (hide && index !== -1 && index > this._lastPinnedIndex) {
 			this.splice('_enrollments', index, 1);
 		}
+		const temp = this._enrollments;
+		this._enrollments = [];
+		this._enrollments = temp;
 
 		if (this._enrollments.length < this._widgetMaxCardVisible && this._nextEnrollmentEntityUrl) {
 			this._entityStoreFetch(this._nextEnrollmentEntityUrl)
