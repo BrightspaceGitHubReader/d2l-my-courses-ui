6a64c316c15b16dd8414a113c6fd00042dba8fc3
Cheick Cisse <Cheick.Cisse@D2L.com>
2019-05-13 11:59:04 -0400

    US106667 Use propreties

--- d2l-my-courses.js
+++ d2l-my-courses.js
@@ -26,7 +26,6 @@ $_documentContainer.innerHTML = `<dom-module id="d2l-my-courses">
 				class="[[class]]"
 				enrollments-url="[[enrollmentsUrl]]"
 				advanced-search-url="[[advancedSearchUrl]]"
-				id="[[id]]"
 				standard-semester-name="[[standardSemesterName]]"
 				standard-department-name="[[standardDepartmentName]]"
 				show-course-code="[[showCourseCode]]"
@@ -37,6 +36,7 @@ $_documentContainer.innerHTML = `<dom-module id="d2l-my-courses">
 				show-semester="[[showSemester]]"
 				org-unit-type-ids="[[orgUnitTypeIds]]"
 				updated-sort-logic="[[updatedSortLogic]]"
+				course-image-upload-cb="[[courseImageUploadCb]]"
 				token="[[token]]">
 			</d2l-my-courses-legacy>
 		</template>
@@ -45,7 +45,6 @@ $_documentContainer.innerHTML = `<dom-module id="d2l-my-courses">
 				class="[[class]]"
 				enrollments-url="[[enrollmentsUrl]]"
 				advanced-search-url="[[advancedSearchUrl]]"
-				id="[[id]]"
 				standard-semester-name="[[standardSemesterName]]"
 				standard-department-name="[[standardDepartmentName]]"
 				show-course-code="[[showCourseCode]]"
@@ -56,6 +55,7 @@ $_documentContainer.innerHTML = `<dom-module id="d2l-my-courses">
 				show-semester="[[showSemester]]"
 				org-unit-type-ids="[[orgUnitTypeIds]]"
 				updated-sort-logic="[[updatedSortLogic]]"
+				course-image-upload-cb="[[courseImageUploadCb]]"
 				token="[[token]]">
 			</d2l-my-courses-container>
 		</template>
@@ -66,7 +66,40 @@ $_documentContainer.innerHTML = `<dom-module id="d2l-my-courses">
 document.head.appendChild($_documentContainer.content);
 Polymer({
 	is: 'd2l-my-courses',
+	properties: {
+		// URL that directs to the advanced search page
+		advancedSearchUrl: String,
+		// URL that is called by the widget to fetch enrollments
+		enrollmentsUrl: String,
+		// URL that is called by the widget to fetch results from the course image catalog
+		imageCatalogLocation: String,
+		// Configuration value passed in to toggle course code -- passed to animation tile
+		showCourseCode: Boolean,
+		// Configuration value passed in to toggle Learning Paths code
+		orgUnitTypeIds: String,
+		// Configuration value passed in to toggle semester on course tile -- passed to animation tile
+		showSemester: Boolean,
+		// Standard Semester OU Type name to be displayed in the all-courses filter dropdown
+		standardDepartmentName: String,
+		// Standard Department OU Type name to be displayed in the all-courses filter dropdown
+		standardSemesterName: String,
+		// Types of notifications to include in update count in course tile -- passed to animation tile
+		courseUpdatesConfig: Object,
+		// Callback for upload in image-selector
+		courseImageUploadCb: Function,
+		// URL to fetch promoted searches for tabs
+		promotedSearches: String,
+		// URL to fetch a user's settings (e.g. default tab to select)
+		userSettingsUrl: String,
+		// URL to fetch widget settings
+		presentationUrl: String,
+		// Feature flag (switch) for using the updated sort logic and related fetaures
+		updatedSortLogic: {
+			type: Boolean,
+			value: false
+		}
+	},
 	behaviors: [
-		D2L.MyCourses.MyCoursesBehavior
+		D2L.PolymerBehaviors.Siren.EntityBehavior
 	]
 });
