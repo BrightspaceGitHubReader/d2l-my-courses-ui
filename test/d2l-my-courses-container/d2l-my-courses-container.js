ec2651ea3e76676c2f563e673d26e02d9aef929a
Edward Gao <Edward.Gao@D2L.com>
2019-08-12 15:35:37 -0400

    DE35094-fix broken test

--- test/d2l-my-courses-container/d2l-my-courses-container.js
+++ test/d2l-my-courses-container/d2l-my-courses-container.js
@@ -117,6 +117,7 @@ describe('d2l-my-courses', () => {
 		component._userSettingsEntity = new UserSettingsEntity(lastSearchResponse);
 		component._promotedSearchEntity = new PromotedSearchEntity(promotedSearchResponse);
 		component._enrollmentCollectionEntity = new EnrollmentCollectionEntity(enrollmentsSearchResponse);
+		component.attached();
 	});
 
 	afterEach(() => {
@@ -223,7 +224,7 @@ describe('d2l-my-courses', () => {
 	describe('Public API', () => {
 		it('should call d2l-my-courses-content.courseImageUploadCompleted when not grouped by tab', done => {
 			component.updatedSortLogic = true;
-			component.promotedSearches = null;
+			component._hasTabActions = false;
 			flush(() => {
 				var stub = sandbox.stub(component.$$('d2l-my-courses-content'), 'courseImageUploadCompleted');
 				component.courseImageUploadCompleted();
@@ -234,7 +235,7 @@ describe('d2l-my-courses', () => {
 
 		it('should call d2l-my-courses-content.getLastOrgUnitId when not grouped by tab', done => {
 			component.updatedSortLogic = true;
-			component.promotedSearches = null;
+			component._hasTabActions = false;
 			flush(() => {
 				var stub = sandbox.stub(component.$$('d2l-my-courses-content'), 'getLastOrgUnitId');
 				component.getLastOrgUnitId();
