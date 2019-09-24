ac05d10066102128e8bcbbd46d43ae061e248ac2
Edward Gao <Edward.Gao@D2L.com>
2019-09-24 16:51:58 -0400

    DE36168-fix the legacy tests

--- test/legacy/d2l-filter-menu-legacy/d2l-filter-list-item-legacy.js
+++ test/legacy/d2l-filter-menu-legacy/d2l-filter-list-item-legacy.js
@@ -38,14 +38,14 @@ describe('d2l-filter-list-item-legacy', function() {
 
 	it('should show the unchecked icon when the item is not selected', function() {
 		listItem.selected = false;
-		expect(listItem.$$('d2l-icon.icon-checked').getComputedStyleValue('display')).to.equal('none');
-		expect(listItem.$$('d2l-icon.icon-unchecked').getComputedStyleValue('display')).to.not.equal('none');
+		expect(window.getComputedStyle(listItem.$$('d2l-icon.icon-checked'), null).getPropertyValue('display')).to.equal('none');
+		expect(window.getComputedStyle(listItem.$$('d2l-icon.icon-unchecked'), null).getPropertyValue('display')).to.not.equal('none');
 	});
 
 	it('should show the checked icon when the item is selected', function() {
 		listItem.selected = true;
-		expect(listItem.$$('d2l-icon.icon-unchecked').getComputedStyleValue('display')).to.equal('none');
-		expect(listItem.$$('d2l-icon.icon-checked').getComputedStyleValue('display')).to.not.equal('none');
+		expect(window.getComputedStyle(listItem.$$('d2l-icon.icon-checked'), null).getPropertyValue('display')).to.not.equal('none');
+		expect(window.getComputedStyle(listItem.$$('d2l-icon.icon-unchecked'), null).getPropertyValue('display')).to.equal('none');
 	});
 
 	it('should fetch the organization when the enrollment changes', function(done) {
