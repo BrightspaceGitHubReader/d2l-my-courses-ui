9dd775ed442a2e455aedc12edf1f6ce7c55a438c
Edward Gao <Edward.Gao@D2L.com>
2019-09-24 16:38:59 -0400

    DE36168-fix the tests

--- test/d2l-filter-menu/d2l-filter-list-item-role.js
+++ test/d2l-filter-menu/d2l-filter-list-item-role.js
@@ -7,13 +7,13 @@ describe('d2l-filter-list-item-role', function() {
 
 	it('should show the unchecked icon when the item is not selected', function() {
 		listItem.selected = false;
-		expect(listItem.$$('d2l-icon.icon-checked').getComputedStyleValue('display')).to.equal('none');
-		expect(listItem.$$('d2l-icon.icon-unchecked').getComputedStyleValue('display')).to.not.equal('none');
+		expect(window.getComputedStyle(listItem.$$('d2l-icon.icon-checked'), null).getPropertyValue('display')).to.equal('none');
+		expect(window.getComputedStyle(listItem.$$('d2l-icon.icon-unchecked'), null).getPropertyValue('display')).to.not.equal('none');
 	});
 
 	it('should show the checked icon when the item is selected', function() {
 		listItem.selected = true;
-		expect(listItem.$$('d2l-icon.icon-unchecked').getComputedStyleValue('display')).to.equal('none');
-		expect(listItem.$$('d2l-icon.icon-checked').getComputedStyleValue('display')).to.not.equal('none');
+		expect(window.getComputedStyle(listItem.$$('d2l-icon.icon-checked'), null).getPropertyValue('display')).to.not.equal('none');
+		expect(window.getComputedStyle(listItem.$$('d2l-icon.icon-unchecked'), null).getPropertyValue('display')).to.equal('none');
 	});
 });
