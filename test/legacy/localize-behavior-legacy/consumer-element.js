ba2d6427630039fe96cdd06eb4633a2cef815f47
Stacey Van Herk <Stacey.VanHerk@D2L.com>
2020-03-25 15:07:01 -0400

    Update eslint version, move legacy tests to allow for separate eslint files for future (and for easier deletion later)

--- test/legacy/localize-behavior-legacy/consumer-element.js
+++
@@ -1,8 +0,0 @@
-import '../../../legacy/localize-behavior-legacy.js';
-import { Polymer } from '@polymer/polymer/lib/legacy/polymer-fn.js';
-Polymer({
-	is: 'consumer-element',
-	behaviors: [
-		D2L.PolymerBehaviors.MyCourses.LocalizeBehaviorLegacy
-	]
-});
