ca1698cf783eb66f05e8aba66d31df753a25b009
Stacey Van Herk <Stacey.VanHerk@D2L.com>
2020-04-06 16:25:06 -0400

    Revert changes to the perf test

--- test/d2l-my-courses-content/d2l-my-courses-content.js
+++ test/d2l-my-courses-content/d2l-my-courses-content.js
@@ -616,25 +616,24 @@ describe('d2l-my-courses-content', () => {
 
 		beforeEach(() => {
 			stub = sandbox.stub(component, 'performanceMeasure');
-			flush();
 		});
 
 		it('should measure d2l.my-courses when all visible course tile images have loaded', done => {
-			component.addEventListener('course-image-loaded', () => {
+			var listener = () => {
+				component.removeEventListener('initially-visible-course-tile', listener);
+				component.dispatchEvent(new CustomEvent('course-image-loaded'));
+			};
+			component.addEventListener('initially-visible-course-tile', listener);
+
+			component.dispatchEvent(new CustomEvent('initially-visible-course-tile'));
+
+			setTimeout(() => {
 				expect(stub).to.have.been.calledWith(
 					'd2l.my-courses',
 					'd2l.my-courses.attached',
 					'd2l.my-courses.visible-images-complete'
 				);
 				done();
-			});
-			component.addEventListener('initially-visible-course-tile', () => {
-				requestAnimationFrame(() => {
-					component.dispatchEvent(new CustomEvent('course-image-loaded'));
-				});
-			});
-			requestAnimationFrame(() => {
-				component.dispatchEvent(new CustomEvent('initially-visible-course-tile'));
 			});
 		});
 
