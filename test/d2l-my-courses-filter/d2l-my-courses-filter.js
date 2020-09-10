663e3aed00852a282eb98e0fbf9b76c722001e87
Stacey Van Herk <Stacey.VanHerk@D2L.com>
2020-09-10 10:27:44 -0400

    Remove requestAnimationFrame as Safari does not liek it at all in this situation

--- test/d2l-my-courses-filter/d2l-my-courses-filter.js
+++ test/d2l-my-courses-filter/d2l-my-courses-filter.js
@@ -17,9 +17,6 @@ function sirenParse(entity) {
 }
 
 function timeout(ms) {
-	if (ms === 0) {
-		return new Promise(requestAnimationFrame);
-	}
 	return new Promise(resolve => setTimeout(resolve, ms));
 }
 
